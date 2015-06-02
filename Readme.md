dca87be200452baf2aacaae6b655926f8d521955
Michael Bastos <bastosmichael@gmail.com>
2015-06-02 13:05:22 -0700

    Think I figured it out

---
+++ Readme.md
@@ -0,0 +1,7 @@
+### Benchmark Elasticsearch vs InfluxDB for Timeseries
+
+Comparison:
+         influx_add_data:      160.8 i/s
+  elasticsearch_add_data:      144.4 i/s - 1.11x slower
+elasticsearch_query_data:       52.9 i/s - 3.04x slower
+       influx_query_data:       23.7 i/s - 6.78x slower
--- test.rb
+++ test.rb
@@ -1,56 +1,41 @@
 require 'benchmark/ips'
 require 'elasticsearch'
 require 'influxdb'
+require 'humanize'
 
 elasticsearch_client = Elasticsearch::Client.new log: true
 influxdb_client = InfluxDB::Client.new 'test', username: 'test', password: 'test'
 index = 'test'
 
 Benchmark.ips do |x|
-  # Configure the number of seconds used during
-  # the warmup phase (default 2) and calculation phase (default 5)
   x.config(:time => 0.01, :warmup => 2)
-
-  # These parameters can also be configured this way
-  # x.time = 1
-  # x.warmup = 2
 
   x.report('elasticsearch_add_data') do
     [*1...10].each do |i|
-      elasticsearch_client.index index: index, type: index, id: i, body: { title: "Test #{i}" }
+      elasticsearch_client.index index: index, type: index, id: i.humanize, body: { title: "Test #{i.humanize}", timestamp: Time.now.to_i }
       # elasticsearch_client.indices.refresh index: index
     end
   end
 
   x.report('influx_add_data') do
     [*1...10].each do |i|
-      influxdb_client.write_point(i.to_s, { title: "Test #{i}" })
+      influxdb_client.write_point(i.humanize, { title: "Test #{i.humanize}", time: Time.now.to_i })
     end
   end
 
-  # # Typical mode, runs the block as many times as it can
-  # x.report("addition") { 1 + 2 }
+  x.report('elasticsearch_query_data') do
+    [*1...10].each do |i|
+      puts elasticsearch_client.search index: index, body: { query: { match: { title: "Test #{i.humanize}"  } } }
+    end
+  end
 
-  # # To reduce overhead, the number of iterations is passed in
-  # # and the block must run the code the specific number of times.
-  # # Used for when the workload is very small and any overhead
-  # # introduces incorrectable errors.
-  # x.report("addition2") do |times|
-  #   i = 0
-  #   while i < times
-  #     1 + 2
-  #     i += 1
-  #   end
-  # end
+  x.report('influx_query_data') do
+    [*1...10].each do |i|
+      influxdb_client.query "select * from #{i.humanize}" do |name, points|
+        puts "#{name} => #{points}"
+      end
+    end
+  end
 
-  # # To reduce overhead even more, grafts the code given into
-  # # the loop that performs the iterations internally to reduce
-  # # overhead. Typically not needed, use the |times| form instead.
-  # x.report("addition3", "1 + 2")
-
-  # # Really long labels should be formatted correctly
-  # x.report("addition-test-long-label") { 1 + 2 }
-
-  # # Compare the iterations per second of the various reports!
   x.compare!
 end
